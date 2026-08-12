d664ecacd5ae27d99193cba1b8d228ed93e04560
david <davidmalak60@gmail.com>
2026-08-12 14:54:08 +0300

    task1

--- lib/task_section3/task1.dart
+++ lib/task_section3/task1.dart
@@ -1,10 +1,32 @@
-void  main(){
-  print("Hello, World!");
+///student grades
+void main() {
+  // 1. Create a Map containing 5 students and their grades
+  Map<String, int> students = {
+    "David": 90,
+    "John": 80,
+    "Mark": 85,
+    "Peter": 60,
+    "Michael": 95,
+  };
+
+  int total = 0;
+
+  // Using for loop
+  for (var student in students.entries) {
+    // 1. Print each student's name and grade
+    print("${student.key}: ${student.value}");
+
+    // 2. Print only students who scored 80 or higher
+    if (student.value >= 80) {
+      print("${student.key} scored 80 or higher");
+    }
+
+    // Calculate total grades
+    total += student.value;
+  }
+  print("-------------------------------");
+  // 3. Calculate and print average
+  double average = total / students.length;
+
+  print("Average grade: $average");
 }
-<<<<<<< HEAD
-///dd
-///dddddd
-///ddddddd
-=======
-///helll
->>>>>>> 6486b1bd6b80a39626c41c175673d9b3334aa067
